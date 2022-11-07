c98ccaa16c2603851c52a6a6a71b77a3db607f9c
WiNMasTeR-SW <sungwoong@4thevision.com>
2022-11-07 14:13:33 +0900

    20221107 #6 - test end

--- src/main/java/com/eura/web/config/SecurityConfig.java
+++ src/main/java/com/eura/web/config/SecurityConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // @Autowired
     // private UserService userService;
 
@@ -25,6 +26,7 @@ public class SecurityConfig {
 //         web.ignoring().antMatchers("/resources/static/**");
 //     }
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
 /*        http.authorizeRequests()
