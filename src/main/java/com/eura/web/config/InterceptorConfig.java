e3368271126912d4c8f40465584faa5815c2c43d
WiNMasTeR-SW <sungwoong@4thevision.com>
2022-11-07 15:31:13 +0900

    20221107 - Security RC1

---
+++ src/main/java/com/eura/web/config/InterceptorConfig.java
@@ -0,0 +1,28 @@
+/**
+ * @author Woong Sung(WiNMasTeR)
+ * 인터셉터 설정
+ */
+package com.eura.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.eura.web.config.Interceptor.ApiAuthorityInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApiAuthorityInterceptor apiAuthorityInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+            .addInterceptor(apiAuthorityInterceptor)
+        	.addPathPatterns("/meet/**","/live/**")
+            .excludePathPatterns("/*");
+    }
+}
--- src/main/java/com/eura/web/config/SecurityConfig.java
+++ src/main/java/com/eura/web/config/SecurityConfig.java
@@ -2,41 +2,45 @@ package com.eura.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    // @Autowired
-    // private UserService userService;
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-//     @Override
-//     public void configure(WebSecurity web) throws Exception {
-//         web.ignoring().antMatchers("/resources/static/**");
-//     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-/*        http.authorizeRequests()
-                .antMatchers("/").authenticated()
-                .anyRequest().authenticated();*/
-        http.httpBasic().disable()
-        .csrf().disable()//csrf 미적용
-        .authorizeRequests().antMatchers("/").permitAll()
-        .anyRequest().permitAll()
-            .and().exceptionHandling().accessDeniedPage("/denied");
+        http
+            .httpBasic().disable()
+            .csrf().disable()   //csrf 미적용
+            .formLogin().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .authorizeRequests()
+                    .antMatchers("/meet","/live").authenticated()
+                    .anyRequest().permitAll()
+                .and()
+                    .exceptionHandling().accessDeniedPage("/denied");
 
         // http.formLogin()
         //         .loginPage("/login")
